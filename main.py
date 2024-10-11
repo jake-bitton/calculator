d32746a63c4af610c984352cf58bb28f034d533a
Jake Bitton <142925570+jake-bitton@users.noreply.github.com>
2024-10-11 13:54:52 -0400

    updated

--- main.py
+++ main.py
@@ -14,12 +14,42 @@ def multiply(a: int | float, b: int | float) -> int | float:
 def divide(a: int | float, b: int | float) -> int | float:
     return a/b
 
-def exponent(a: int|float, power: int) -> int | float:
-    #  Need to add functionality to exponentiate by fractional/decimal values
-    exp = a
-    for i in range(power-1):
-        exp *= exp*a
-    return exp
+def exponent(a: int|float, power: int|float) -> int | float:
+    '''
+    Calculates result of a^(power), 
+    if power is an integer simply mulitplies a by itself power times 
+    (result initially set to 1, i.e a^0 for all a)
+
+    else if power is a float it splits it into its parts (integer and decimal)
+    such that a^(power) = a^(int_power) + a^(float_power) it then recursively calls 
+    exponent() on the integer portion of the equation and 
+    [INSERT EXPLANATION FOR DECIMAL CALCULATION HERE]
+    '''
+    if power.__class__ != int:
+        str_power = str(power)
+        int_power = ''  #  Integer part of power
+        float_power = ''  #  Decimal/float part of power
+        #  splits power into integer and decimal/float parts allowing equation to be split into a^(float: power) = a^(int_power) + a^(float_power)
+        for i in range(len(str_power)-1):
+            if power[i] != '.':
+                int_power += power[i]
+            else:
+                float_power = str_power[i:]
+        int_power = int(int_power)
+        float_power = float(float_power)
+        result = exponent(a, int_power)  #  Recursive (?) call to exponent that returns the a^(int_power) part of equation
+        #  Logic for float/decimal portion goes here
+    else:
+        #  Initializes result as a^0 and then multiplies it by a for power times and returns
+        result = 1  #  Sets result == a^0 for all a
+        for i in range(power):
+            result *= a
+        return result
+
+
+
+            
+
 
 def n_root(a: int|float, n: int|float) -> int|float:
     #  To be implemented
